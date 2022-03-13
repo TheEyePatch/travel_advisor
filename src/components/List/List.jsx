c587a83c6ebeb7b866402f37445b481064927055
Leif Bersamina <leifbersamina@gmail.com>
2022-03-13 21:07:38 +0800

    add static List component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './List.module.css'
 
 function List(){
+  
+  const [places, setPlaces] = useState([])
+
+  useEffect(() => {
+    let data = [
+      {
+        name: 'Baguio Restaurant One',
+        timezone: 'PH TimeZone'
+      },
+      {
+        name: 'Baguio Restaurant Two',
+        timezone: 'PH TimeZone'
+      },
+      {
+        name: 'Baguio Restaurant Three',
+        timezone: 'PH TimeZone'
+      }
+    ]
+
+    setPlaces(data)
+  }, [])
+
   return (
     <div className={styles.container}>
-      <h4>Restaurants, Hotels & Attractions around you</h4>
-    </div>
+      <h3>Restaurants, Hotels & Attractions around you</h3>
+      {
+        places?.map(place => {
+          return (
+            <li key={place.name}>
+              <h1>{place.name}</h1>
+              <p>{place.timezone}</p>
+            </li>
+          )
+        })
+      }
+    </div> 
   )
 }
 
