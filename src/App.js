32ad17c703cfcbd103e552fba8ae4b1c6aece125
Leif Bersamina <leifbersamina@gmail.com>
2022-04-16 00:04:26 +0800

    add travel advisor api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './components/Header/Header'
 import List from './components/List/List'
 import Map from './components/Map/Map'
 import styles from './App.module.css'
 
+import { getHotelsData } from './api/index'
+
 function App(){
+  const [hotels, setHotels] = useState([]);
+
+  const getHotels = async () => {
+    const data = await getHotelsData()
+    console.log(data)
+    setHotels(data)
+  }
+  useEffect(() => {
+    getHotels()
+  }, [])
+
   return (
     <div className={styles.main}>
       <Header/>
---
+++ src/api/index.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const getHotelsData = async () => {
+  try {
+    const options = {
+      params: {
+        bl_latitude: '11.847676',
+        bl_longitude: '108.473209',
+        tr_longitude: '109.149359',
+        tr_latitude: '12.838442',
+        limit: '30',
+      },
+      headers: {
+        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+        'X-RapidAPI-Key': '46ffed2257msh47d4b862b182534p112d14jsn74844e541f9d'
+      }
+    };
+    const responseAPI = await axios.get('https://travel-advisor.p.rapidapi.com/hotels/list-in-boundary', options);
+
+    return responseAPI.data
+  }catch(error){
+    console.log(error)
+  }
+}
