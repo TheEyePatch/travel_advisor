276ed79792688e752618a41e3a4cea9d379f859a
Leif Bersamina <leifbersamina@gmail.com>
2022-04-16 22:15:24 +0800

    add useState hooks and comment out api call temporarily

--- src/App.js
+++ src/App.js
@@ -4,26 +4,34 @@ import List from './components/List/List'
 import Map from './components/Map/Map'
 import styles from './App.module.css'
 
-import { getHotelsData } from './api/index'
+import { getPlacesData } from './api/index'
 
 function App(){
-  const [hotels, setHotels] = useState([]);
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({lat: 14.630473624869978, lng: 481.02126039567804})
+  const [bounds, setBounds] = useState(null)
 
-  const getHotels = async () => {
-    const data = await getHotelsData()
-    console.log(data)
-    setHotels(data)
-  }
-  useEffect(() => {
-    getHotels()
-  }, [])
+  // const getHotels = async () => {
+  //   const data = await getPlacesData()
+  //   console.log(data)
+  //   setPlaces(data)
+  // }
+  // useEffect(() => {
+  //   getHotels()
+  // }, [])
 
   return (
     <div className={styles.main}>
       <Header/>
       <div className={styles.data_section}>
         <List className={styles.list_section}/>
-        <Map className={styles.google_map_section}/>
+        <Map 
+          className={styles.google_map_section}
+          setCoordinates={setCoordinates}
+          setBounds={setBounds}
+          coordinates={coordinates}
+          bounds={bounds}
+        />
       </div>
       
     </div>
