4aa9a4b8888fbc69c01dd893c0d3e15c722802d0
Leif Bersamina <leifbersamina@gmail.com>
2022-05-01 01:40:36 +0800

    adjust styling of header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Autocomplete } from '@react-google-maps/api';
 import  styles from './Header.module.css'
+import { Input } from 'antd';
+const { Search } = Input;
 
 function Header(){
+  const onSearch = ()=>{}
+  const searchStyle = {
+    width: 300,
+    color: "#FAEBD7",
+    backgroundColor: "#00FFFF"
+  }
   return (
     <div className={styles.header}>
       <h4 className={styles.title}>Travel Advisor</h4>
 
       <div className={styles.search_box}>
         <h6 className={styles.title}>Explore New Places</h6>
-        <div className={styles.search}>
-          <input type="search" placeholder='Search...' />
-        </div>
+        <Search
+          className='search_bar'
+          placeholder="input search text" 
+          onSearch={onSearch}
+          style={searchStyle}
+          size='large'
+          />
       </div>
     </div>
   )
